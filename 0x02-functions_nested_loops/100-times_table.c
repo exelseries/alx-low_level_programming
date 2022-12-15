5a43fb57d4a413822bf326196c24e77b7e8ee4f2
exelseries <39970611+exelseries@users.noreply.github.com>
2022-12-15 14:54:01 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,12 @@
-
-
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+
+	return (0);
+}
